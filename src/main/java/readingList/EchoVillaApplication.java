8d073ddfb90039986538c830a165d3948ee0e900
akila weeratunga <akilaweeratunga@gmail.com>
2017-05-01 19:17:29 +0530

    initial commit

---
+++ src/main/java/readingList/EchoVillaApplication.java
@@ -0,0 +1,12 @@
+package readingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoVillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoVillaApplication.class, args);
+	}
+}
